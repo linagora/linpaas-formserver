e1b1a2d55b51c55e0adc9e628d7b9411f0ab8959
Graham Crosmarie <gcrosmarie@linagora.com>
2013-05-14 11:38:23 +0200

    added sample request for form deletion

---
+++ samples/delete.js
@@ -0,0 +1,38 @@
+//
+// Form deletion request
+//
+// Graham Crosmarie - gcrosmarie@linagora.com
+//
+
+var request = require('request')
+  , config = require('../app/config')
+  , readline = require('readline');
+
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Input a form id :", function(answer) {
+  console.log("You asked to delete form with id : ", answer);
+
+	request({
+		 uri: 'http://' + config.host + ':' + config.port + '/forms/' + answer,
+		 method: 'delete',
+	  }, function(err, res, body) {
+		 if (err) {
+		   console.log(err)
+		 } else {
+		   console.log(res.statusCode)
+		   if (res.statusCode == 200) {
+		     console.log('Form Deleted')
+		   } else {
+		     console.log('Bad return code')
+		   }
+		 }
+	  }
+	);
+
+  rl.close();
+});
